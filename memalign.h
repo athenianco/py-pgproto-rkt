ada72d27a9b9abbb10db985d60723bbe4eb68980
Vadim Markovtsev <vadim@athenian.co>
2022-05-31 22:10:37 +0200

    [DEV-4240] Support macOS

--- memalign.h
+++ memalign.h
@@ -1,8 +1,18 @@
 #include <malloc.h>
+#include <stdlib.h>
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
 #define memalign(alignment, size) _aligned_malloc(size, alignment)
 #define aligned_free _aligned_free
 #else
+#if __APPLE__
+inline void *memalign(size_t alignment, size_t size) {
+  void *mem;
+  if (!posix_memalign(&mem, alignment, size)) {
+    return NULL;
+  }
+  return mem;
+}
+#endif
 #define aligned_free free
 #endif
